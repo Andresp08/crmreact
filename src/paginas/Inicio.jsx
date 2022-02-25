55be8dd323e101bd7d67644cc1fec570a5557ea8
Andres Felipe <Andresfp29032001@gmail.com>
2022-02-25 10:13:39 -0500

    Routing and layouts starting

---
+++ src/paginas/Inicio.jsx
@@ -0,0 +1,11 @@
+
+
+const Inicio = () => {
+  return (
+    <h1>
+      Inicio Cliente
+    </h1>
+  )
+}
+
+export default Inicio
---
+++ src/paginas/NuevoCliente.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const NuevoCliente = () => {
+  return (
+    <h1>
+      Nuevo Cliente
+    </h1>
+  )
+}
+
+export default NuevoCliente
